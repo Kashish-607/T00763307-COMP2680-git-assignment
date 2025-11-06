556447d92d017fa5f959a65e094cb8096d3dec4c
Kashish-607 <kashishmakwana607@gmail.com>
2025-11-05 22:09:08 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple message when page loads
+console.log("Page loaded successfully! Hello from script.js");
+
+// Example of interactive feature
+document.querySelector('h1').addEventListener('click', function() {
+    console.log("You clicked the heading!");
+    alert("Welcome to my COMP2680 project!");
+});
